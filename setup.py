ebe711d4ced6e9086809c3ff1b58df6366a87305
quentin <quentin.malnatti@gmail.com>
2023-09-26 20:12:57 +0200

    add missing required libs

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     url="https://github.com/QuentinMalnatti/lib-driver",
     author="Quentin Malnatti",
     packages=["personal_web_driver", "personal_web_driver.getters", "personal_web_driver.notifiers"],
-    install_requires=["personal_logger"],
-    version="0.1.2",
+    install_requires=["personal_logger", "selenium", "undetected_chromedriver", "webdriver_manager"],
+    version="0.1.3",
     description="Personal web driver",
     long_description=open("README.md").read(),
 )
