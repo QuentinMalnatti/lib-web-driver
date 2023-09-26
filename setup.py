a31431e972e19d22982beab51b1f39e27879acc8
quentin <quentin.malnatti@gmail.com>
2023-09-26 20:00:48 +0200

    add missing packages

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='PersonalWebDriver',
-    url='https://github.com/QuentinMalnatti/lib-driver',
-    author='Quentin Malnatti',
-    packages=['personal_web_driver'],
-    install_requires=['personal_logger'],
-    version='0.1.1',
-    description='Personal web driver',
-    long_description=open('README.md').read(),
+    name="PersonalWebDriver",
+    url="https://github.com/QuentinMalnatti/lib-driver",
+    author="Quentin Malnatti",
+    packages=["personal_web_driver", "personal_web_driver.getters", "personal_web_driver.notifiers"],
+    install_requires=["personal_logger"],
+    version="0.1.2",
+    description="Personal web driver",
+    long_description=open("README.md").read(),
 )
