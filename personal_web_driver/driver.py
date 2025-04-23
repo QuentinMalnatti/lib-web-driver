8cbac00ff0b40de38871f6f69a07f262eaa99cd4
quentin <quentin.malnatti@gmail.com>
2025-04-23 10:19:17 +0200

    v1.1.6 page source

--- personal_web_driver/driver.py
+++ personal_web_driver/driver.py
@@ -25,9 +25,18 @@ class Driver(object):
         path_driver = ChromeDriverManager().install().replace("THIRD_PARTY_NOTICES.chromedriver", "chromedriver")
         self.__driver = uc.Chrome(driver_executable_path=path_driver, options=options, headless=headless_option)
 
+        self.__current_page_source = None
+
+    def get_current_page_source(self):
+        return self.__current_page_source
+
+    def set_current_page_source(self):
+        self.__current_page_source = self.__driver.page_source
+
     @LoggerDecorator.log(stage="Launch driver")
     def launch(self, url: str):
         self.__driver.get(url=url)
+        self.set_current_page_source()
 
     @LoggerDecorator.log(stage="Quit driver")
     def quit(self):
@@ -36,14 +45,11 @@ class Driver(object):
     @LoggerDecorator.log(stage="Refresh driver")
     def refresh(self):
         self.__driver.refresh()
+        self.set_current_page_source()
 
     @LoggerDecorator.log(stage="Save a screenshot")
     def save_screenshot(self, path_screenshot):
         self.__driver.save_screenshot(path_screenshot)
-
-    @LoggerDecorator.log(stage="get page source")
-    def get_page_source(self):
-        return self.__driver.page_source
 
     def get_element_by_xpath(self, getter: Type[AbstractGetter], method: str, attribute: str, value: str):
         xpath = getter.get_xpath(method=method, attribute=attribute, value=value)
@@ -72,16 +78,20 @@ class Driver(object):
         action = ActionChains(self.__driver)
         action.move_to_element(element).click().perform()
         sleep(sleep_time)
+        self.set_current_page_source()
 
     def open_tab(self, url: str):
         self.__driver.switch_to.new_window('tab')
         self.__driver.get(url=url)
+        self.set_current_page_source()
 
     def switch_tab(self, index: int):
         self.__driver.switch_to.window(self.__driver.window_handles[index])
+        self.set_current_page_source()
 
     def close_tab(self):
         self.__driver.close()
+        self.set_current_page_source()
 
     @staticmethod
     @LoggerDecorator.log(stage="Add some human behavior by running a pause")
